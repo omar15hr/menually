8a9e9518a445fb54b4838aedabefd35902a3fbe5
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2026-04-06 15:53:21 -0400

    form, imagen upload for customization, image

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -7,6 +7,11 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
 import { MultiSelectChips } from "./MultiSelectChips";
+import PhotoUpload from "../shared/PhotoUpload";
+import { cn } from "@/lib/utils";
+import CameraIcon from "../icons/CameraIcon";
+import XIcon from "../icons/XIcon";
+import Image from "next/image";
 
 const OPTIONS = [
   { label: "Picante", value: "picante" },
@@ -17,8 +22,9 @@ const OPTIONS = [
   { label: "Apto APLV", value: "aplv" },
 ];
 
-export default function ProductForm({ product, categoryId }: any) {
-  const [isPending, startTransition] = useState();
+export default function ProductForm({ categoryId }: { categoryId: string }) {
+  const [isPending, setIsPending] = useState();
+  const [productImageUrl, setProductImageUrl] = useState("");
 
   return (
     <form className="flex flex-col gap-6 bg-white p-6 border border-[#E4E4E6] rounded-lg">
@@ -70,9 +76,48 @@ export default function ProductForm({ product, categoryId }: any) {
 
       <div className="flex flex-col gap-2">
         <span className="text-[#58606E] text-sm">Imagen del producto</span>
-        <div className="flex border border-[#E4E4E6] items-center gap-2 p-4 rounded-lg">
-          hola
-        </div>
+        <PhotoUpload imagePath={"products"} onPhotoUploaded={setProductImageUrl}>
+          <div
+            className={cn(
+              "flex gap-4 justify-center rounded-2xl p-4 cursor-pointer transition-colors items-center",
+              productImageUrl
+                ? "border-[#E4E4E6] border-2"
+                : "border-dashed border-[#E4E4E6] border-2",
+            )}
+          >
+            {productImageUrl ? (
+              <Image
+                src={productImageUrl}
+                alt="Image placeholder"
+                width={300}
+                height={300}
+                className="rounded-lg size-15"
+              />
+            ) : (
+              <span className="bg-[#E5E7EA] px-2.5 py-4 rounded-full">
+                <CameraIcon />
+              </span>
+            )}
+            <div className="text-sm w-full">
+              {productImageUrl.length < 0 ? (
+                <div className="">
+                  <h2 className="text-[#1C1C1C] font-semibold">
+                    Sube una imagen
+                  </h2>
+                  <p className="text-[#58606E]">
+                    Recomendado 328 x 200px PNG.
+                  </p>
+                  <span className="text-[#25B205]">Seleccionar archivo</span>
+                </div>
+              ) : (
+                <div className="flex items-center justify-between text-[#58606E]">
+                  <span>Archivo subido</span>
+                  <XIcon />
+                </div>
+              )}
+            </div>
+          </div>
+        </PhotoUpload>
       </div>
 
       <Button
