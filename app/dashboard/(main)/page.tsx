4d33378aa0fb60bce54c052c9355b4a003d90e8e
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2026-05-01 18:35:39 -0400

    mejora de autenticacion

--- app/dashboard/(main)/page.tsx
+++ app/dashboard/(main)/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
 import Header from "@/components/shared/Header";
 import WithMenu from "@/components/dashboard/WithMenu";
 import { getAuthUser } from "@/lib/queries/auth.queries";
@@ -12,10 +10,9 @@ export default async function DashboardPage() {
   const user = await getAuthUser();
 
   const menu = await getMenuByUserId(user.id);
-  if (!menu) redirect("/auth/signin");
 
   const [profile] = await Promise.all([getProfileByUserId(user.id)]);
-  const categories = await getCategoriesByMenuId(menu.id);
+  const categories = menu ? await getCategoriesByMenuId(menu.id) : [];
 
   return (
     <div className="w-full bg-white flex flex-col min-h-screen">
